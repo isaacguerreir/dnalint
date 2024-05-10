a60029a5a0e8b2f2d695158a94f872f41a9a38eb
isaacguerreir <isaacguerreirocom@gmail.com>
2024-05-10 12:00:43 -0300

    remove everything

--- src/error.ts
+++
@@ -1,8 +0,0 @@
-export const no_sequence_err = `Configuration doesn't have a sequence field to be verified. Try adding:
-	{
-		sequence: "ATAC..."
-		// ...rest of configuration
-	}
-`
-
-export const rule_not_found = (identifier: string) => `Rule ${identifier} not found. Are you sure this rule is available at your configuration?`
